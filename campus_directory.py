8f4fa2843399335c833f4c6c94ebab69153e470d
Frederik Stensaeth <stensaethf@carleton.edu>
2016-01-21 12:17:22 -0600

    scrape updated to handle logins

--- campus_directory.py
+++ campus_directory.py
@@ -1,19 +1,21 @@
-import requests
 from bs4 import BeautifulSoup as BS4
 import csv
 import os
 import sys
+import commands
+import time
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
 def scraper(m, s):
     for number in range(26):
+        print number
+        time.sleep(0.1)
 
         letters = m + chr(97 + number)
-        # HOW TO LOG IN? NEED TO DO THAT TO GET ALL INFORMATION FROM SITE.
-        search_result = requests.get(campus_directory % letters)
-        soup = BS4(search_result.content)
+        html = commands.getoutput("ENTER curl bla bla bla here")
+        soup = BS4(html)
 
         if(soup.find(attrs={"id":"discoErrorNotice"})!=None): continue
 
@@ -24,8 +26,6 @@ def scraper(m, s):
             continue
         else:
             for person in soup.find_all(attrs={"class":"person"}):
-                print count
-                count = count + 1
 
                 name = ""
                 year = ""
@@ -108,7 +108,8 @@ def scraper(m, s):
                         group1 = heading_groups.find(attrs={"class":"group1"})
                         if group1 != None:
                             name = group1.h2.get_text()
-                            year = group1.find(attrs={"class":"affiliation"}).get_text()
+                            if group1.find(attrs={"class":"affiliation"}) != None:
+                                year = group1.find(attrs={"class":"affiliation"}).get_text()
                             majors = group1.find(attrs={"class":"majors"})
                             if majors != None:
                                 cross_ref = majors.find(attrs={"class":"crossRef"})
@@ -120,7 +121,8 @@ def scraper(m, s):
                             group1 = heading_groups.find(attrs={"class":"group1"})
                             if group1 != None:
                                 name = group1.h2.get_text()
-                                year = group1.find(attrs={"class":"affiliation"}).get_text()
+                                if group1.find(attrs={"class":"affiliation"}) != None:
+                                    year = group1.find(attrs={"class":"affiliation"}).get_text()
                                 majors = group1.find(attrs={"class":"majors"})
                                 if majors != None:
                                     major = group1.find(attrs={"class":"major"}).get_text()
@@ -142,25 +144,8 @@ def scraper(m, s):
                         person_dict[person_stats] = 1
 
 person_dict = {}
-count = 0
 
-if len(sys.argv) != 3:
-    print "$ <username> <password>"
-
-campus_directory = "https://apps.carleton.edu/campus/directory/?first_name=%s&search_for=student"
-# Use 'with' to ensure the session context is closed after use.
-payload = {"username" : sys.argv[1], "password" : sys.argv[2]}
-with requests.Session() as s:
-    CARLETONedu = Xx # Enter
-    CARLETONpass = Xx # Enter
-    auth = (CARLETONedu, CARLETONpass)
-    s.post("https://apps.carleton.edu/login/", auth=auth)
-    # print the html returned or something more intelligent to see if it's a successful login page.
-
-    # An authorised request.
-    scraper("", s)
-
-############## MORE HERE #########
+scraper("", 0)
 
 csv_list = []
 csv_list.append(["Name", "Year", "Major", "Residence Hall", "Email", "Telephone", "Home Address", "Tags", "Urls"])
@@ -169,7 +154,7 @@ for index in person_dict:
     csv_list.append(info_bloc)
 
 if len(csv_list) != 1:
-    filename = "TEST_CAMPUS_DOWNLAOD"
+    filename = "CAMPUS_DOWNLOAD_NEW"
     myfile = open(filename + "-unfinished.csv", 'wb')
     wr = csv.writer(myfile, dialect='excel', quotechar='"', quoting=csv.QUOTE_ALL)
     wr.writerows(csv_list)
@@ -181,22 +166,6 @@ else:
     print "Failed to collect any information."
 
 print "Done!"
-
-####################### START #####
-# if EXISTS:
-#     READ FILE INTO DICT
-#     for index in person_dict:
-#         if index not in file_dict:
-#             file_dict[index] = 1
-#     person_dict = file_dict
-####################### END #####
-
-
-# get personal phone
-# base64 encoding fro authentication?
-# get image
-# get student profile
-
 
 
 
